5463cca9dd64797966d570da2b987b1598e44d16
lp6m <lp6m2014@gmail.com>
2021-11-09 00:24:30 +0900

    add tracking

--- darknet_tracking.py
+++ darknet_tracking.py
@@ -15,6 +15,8 @@ def parser():
                         help="video source. If empty, uses webcam 0 stream")
     parser.add_argument("--out_filename", type=str, default="",
                         help="inference video name. Not saved if empty")
+    parser.add_argument("--out_filename2", type=str, default="",
+                        help="tracking video name. Not saved if empty")
     parser.add_argument("--weights", default="yolov4.weights",
                         help="yolo weights path")
     parser.add_argument("--dont_show", action='store_true',
@@ -119,13 +121,56 @@ def video_capture(frame_queue, darknet_image_queue):
         darknet_image_queue.put(img_for_detect)
     cap.release()
 
+import sort
+import numpy as np
+def inference(darknet_image_queue, detections_queue, fps_queue, tracked_queue):
+    def convert(bbox, conf, classid):
+        left_x, top_y, width, height = bbox
+        right_x = left_x + width
+        bottom_y = top_y + height
+        # dummy = 9999999 #?
+        dummy = 0 #?
+        return [left_x, top_y, right_x, bottom_y, conf/100, dummy, classid]
+        # return [left_x/416, top_y/416, right_x/416, bottom_y/416, conf/100]
+    
+    def convert2(bbox):
+        left_x, top_y, right_x, bottom_y = bbox
+        width = right_x - left_x
+        height = bottom_y - top_y
+        return left_x, top_y, width, height
+        # return [left_x*416, top_y*416, width*416, height*416]
 
-def inference(darknet_image_queue, detections_queue, fps_queue):
+    def tracking(pedestrian_tracker, car_tracker, detections):
+        ped_bboxes = []
+        car_bboxes = []
+        for label, conf, bbox in detections:
+            if label == 'pedestrian':
+                ped_bboxes.append(convert(bbox, float(conf), 0))
+            elif label == 'car':
+                car_bboxes.append(convert(bbox, float(conf), 1))
+        ped_tracked_objects = pedestrian_tracker.update(np.array(ped_bboxes))
+        car_tracked_objects = car_tracker.update(np.array(car_bboxes))
+        tracked_objects = []
+        print(ped_tracked_objects.shape)
+        for tracked_ped in ped_tracked_objects:
+            obj_id = tracked_ped[4]
+            tracked_objects.append(('pedestrian', obj_id, convert2(tracked_ped[0:4])))
+
+        for tracked_car in car_tracked_objects:
+            obj_id = tracked_car[4]
+            tracked_objects.append(('car', obj_id, convert2(tracked_car[0:4])))
+        
+        return tracked_objects
+
+    pedestrian_tracker = sort.Sort()
+    car_tracker = sort.Sort()
     while cap.isOpened():
         darknet_image = darknet_image_queue.get()
         prev_time = time.time()
         detections = darknet.detect_image(network, class_names, darknet_image, thresh=args.thresh)
+        tracked_objects = tracking(pedestrian_tracker, car_tracker, detections)
         detections_queue.put(detections)
+        tracked_queue.put(tracked_objects)
         fps = int(1/(time.time() - prev_time))
         fps_queue.put(fps)
         print("FPS: {}".format(fps))
@@ -134,27 +179,69 @@ def inference(darknet_image_queue, detections_queue, fps_queue):
     cap.release()
 
 
-def drawing(frame_queue, detections_queue, fps_queue):
+def drawing(frame_queue, detections_queue, fps_queue, tracked_queue):
+    def draw_tracked_boxes(detections, image):
+        colors = [
+            [255, 71, 71],
+            [235, 142, 71],
+            [235, 210, 71],
+            [188, 235, 71],
+            [118, 235, 71],
+            [71, 235, 96],
+            [71, 235, 164],
+            [71, 235, 235],
+            [71, 96, 235],
+            [118, 71, 235],
+            [188, 71, 235],
+            [235, 71, 210],
+            [235, 71, 142]
+        ]
+        import cv2
+        for label, obj_id, bbox in detections:
+            color = colors[obj_id % len(colors)]
+            # color.reverse() #RGB -> BGR
+            left, top, right, bottom = darknet.bbox2points(bbox)
+            cv2.rectangle(image, (left, top), (right, bottom), color, 3)
+            cv2.putText(image, "{} [id={}, {}%]".format(label, obj_id, confidence),
+                        (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        color, 2)
+        return image
+
     random.seed(3)  # deterministic bbox colors
     video = set_saved_video(cap, args.out_filename, (video_width, video_height))
+    video2 = set_saved_video(cap, args.out_filename2, (video_width, video_height))
     while cap.isOpened():
         frame = frame_queue.get()
         detections = detections_queue.get()
+        tracked_objects = tracked_queue.get()
+
         fps = fps_queue.get()
         detections_adjusted = []
+        tracked_adjusted = []
         if frame is not None:
+            frame2 = frame.copy()
+
             for label, confidence, bbox in detections:
                 bbox_adjusted = convert2original(frame, bbox)
                 detections_adjusted.append((str(label), confidence, bbox_adjusted))
             image = darknet.draw_boxes(detections_adjusted, frame, class_colors)
+
+            for label, obj_id, bbox in tracked_objects:
+                bbox_adjusted = convert2original(frame2, bbox)
+                tracked_adjusted.append((str(label), int(obj_id), bbox_adjusted))
+            image2 = draw_tracked_boxes(tracked_adjusted, frame2)
+
             if not args.dont_show:
                 cv2.imshow('Inference', image)
+                cv2.imshow('Tracking', image2)
             if args.out_filename is not None:
                 video.write(image)
+                video2.write(image2)
             if cv2.waitKey(fps) == 27:
                 break
     cap.release()
     video.release()
+    video2.release()
     cv2.destroyAllWindows()
 
 
@@ -162,6 +249,7 @@ if __name__ == '__main__':
     frame_queue = Queue()
     darknet_image_queue = Queue(maxsize=1)
     detections_queue = Queue(maxsize=1)
+    tracked_queue = Queue(maxsize=1)
     fps_queue = Queue(maxsize=1)
 
     args = parser()
@@ -179,5 +267,5 @@ if __name__ == '__main__':
     video_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     Thread(target=video_capture, args=(frame_queue, darknet_image_queue)).start()
-    Thread(target=inference, args=(darknet_image_queue, detections_queue, fps_queue)).start()
-    Thread(target=drawing, args=(frame_queue, detections_queue, fps_queue)).start()
+    Thread(target=inference, args=(darknet_image_queue, detections_queue, fps_queue, tracked_queue)).start()
+    Thread(target=drawing, args=(frame_queue, detections_queue, fps_queue, tracked_queue)).start()
